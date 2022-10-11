d31546484db6674c16e1c6104fbc9bdcf1dae35c
Luis Delgado <luis.delgadolopez@externos-cl.cencosud.com>
2022-10-11 19:43:58 -0400

    Upload last changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [subjects, setSubjects] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/subjects', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      setSubjects(response)
+    })
+  }, [])
+
+  const student = {
+    name: "Pepito",
+    lastName: "Perez Rojas",
+    age: 22,
+    school: 'Americano',
+  }
+
+  console.log(subjects)
+
+  const getSubjectsWithFormat = () => {
+    return subjects.map((subject, indice) => {
+      if (subjects.length - 1 === indice) {
+        return ` ${subject}.`
+      }
+      return ` ${subject},`
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Datos del estudiante</h1>
+      <p>Nombre: {student.name}</p>
+      <p>Apellidos: {student.lastName}</p>
+      <p>Edad: {student.age}</p>
+      <p>Colegio: {student.school}</p>
+      <p>Materias: {getSubjectsWithFormat()}</p>
     </div>
   );
 }
